fed5f08a662d1a03bcb0c7a9b70fb765f467d9fb
AndyCui <13520061695@163.com>
2018-01-11 14:36:44 +0800

    去掉不需要的代码

--- src/main/java/person/andy/controller/HelloConsumerController.java
+++ src/main/java/person/andy/controller/HelloConsumerController.java
@@ -29,8 +29,7 @@ public class HelloConsumerController {
     }
 
     @RequestMapping(value = "/serviceHello", method = RequestMethod.GET)
-    public String helloAsService(HttpServletRequest request) {
-        String remoteIp = getClientIp(request);
+    public String helloAsService() {
         return "hello as service";
     }
 
@@ -39,42 +38,6 @@ public class HelloConsumerController {
         return helloService.getUser(name, age);
     }
 
-    private static String getClientIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-
-            }
-        } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (int index = 0; index < ips.length; index++) {
-                String strIp = (String) ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
-        }
-        System.err.println("最终获得的客户端ip是{}" + ip);
-        return ip;
-    }
     @RequestMapping(value = "/apply",method = RequestMethod.GET)
     public ResponseObject applyResponseTest(@RequestParam String userName) {
       return helloService.applyResponseTest(userName);
